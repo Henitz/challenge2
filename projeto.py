0e5bd251c3ab3869e980adf836a7c3ad6b40b563
Henitz <33755649+Henitz@users.noreply.github.com>
2024-01-13 18:51:00 -0300

    formatando li

--- projeto.py
+++ projeto.py
@@ -54,6 +54,13 @@ st.markdown(
             border-radius: 10px;
             margin: 10px 0;
         }
+        .custom-container li {
+            text-align: justify; /* Alinha o texto à justificação */  
+        }
+        .custom-container p {
+            text-align: justify; /* Alinha o texto à justificação */  
+        }
+        
     </style>
     """,
     unsafe_allow_html=True
@@ -72,7 +79,7 @@ tab_contents = {
     </div>
     <style>
         .custom-container p {
-            text-align: justify; /* Alinha o texto à justificação */
+            text-align: justify; /* Alinha o texto à justificação */ 
         }
     </style>
     """,
@@ -91,7 +98,7 @@ tab_contents = {
     <div class="custom-container">
         <p>A biblioteca Prophet, desenvolvida pelo Facebook, é uma ferramenta popular e poderosa para previsão de séries temporais. Ela foi projetada para simplificar o processo de criação de modelos de previsão, oferecendo aos usuários uma maneira fácil de gerar previsões precisas e de alta qualidade, mesmo sem um profundo conhecimento em séries temporais ou estatística avançada.</p>
         <p>Aqui estão alguns pontos-chave sobre o Prophet:</p>
-        <ul>
+        <ol>
             <li><strong>Facilidade de Uso:</strong> O Prophet foi desenvolvido para ser acessível e fácil de usar, permitindo que usuários, mesmo sem experiência avançada em séries temporais, possam construir modelos de previsão.</li>
             <li><strong>Componentes Aditivos:</strong> O modelo do Prophet é baseado em componentes aditivos, onde são consideradas tendências anuais, sazonais e efeitos de feriados, além de componentes de regressão.</li>
             <li><strong>Tratamento de Dados Ausentes e Outliers:</strong> O Prophet lida bem com dados ausentes e outliers, reduzindo a necessidade de pré-processamento extensivo dos dados antes da modelagem.</li>
@@ -99,7 +106,7 @@ tab_contents = {
             <li><strong>Estimativa Automática de Intervalos de Incerteza:</strong> O Prophet fornece intervalos de incerteza para as previsões, o que é essencial para compreender a confiabilidade dos resultados.</li>
             <li><strong>Implementação em Python e R:</strong> Está disponível tanto para Python quanto para R, ampliando sua acessibilidade para diferentes comunidades de usuários.</li>
             <li><strong>Comunidade Ativa e Documentação Detalhada:</strong> A biblioteca possui uma comunidade ativa de usuários e desenvolvedores, além de uma documentação detalhada e exemplos práticos que ajudam na aprendizagem e na solução de problemas.</li>
-        </ul>
+        </ol>
     </div>
     """,
     "Sobre o Autor": """
